36ed11c0dab18fbe25f722a786e35bd995b30658
Deepak <dk6558415@gmail.com>
2025-01-15 01:05:23 +0530

    Add

--- app.js
+++ app.js
@@ -90,6 +90,8 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
+
 app.use((req, res, next) =>{
         res.locals.success = req.flash("success");
         res.locals.error = req.flash("error");
@@ -125,6 +127,8 @@ app.use((err, req, res, next) =>{
         // res.status(statusCode).send(message);
 
 });
+
+
 
 app.listen(8080, () =>{
           console.log("server is listening to port 8080");
